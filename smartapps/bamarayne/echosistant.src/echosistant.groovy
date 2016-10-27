36e7155e4b640e83c0727ef8c080bbe429444467
Jason Headley <alabamaslammer21@gmail.com>
2016-10-27 10:40:37 -0500

    Working

--- smartapps/bamarayne/echosistant.src/echosistant.groovy
+++ smartapps/bamarayne/echosistant.src/echosistant.groovy
@@ -5,11 +5,22 @@
  *		10/26/2016  version 0.1.2a      Name change
  *		10/26/2016	version 0.1.2		Added Icons by @SBDOBRESCU.
  *		10/25/2016	version 0.1.1		Bug Fix in the Token Renew process
- *		10/25/2016  version 0.1.0		Alpha Testing of code complete.
+ *  	10/25/2016  version 0.1.0		Alpha Testing of code complete.
+ *		10/23/2016	version 0.0.2		Restrictions operational (modes/days/hours), Sonos operational.		
+ *		10/23/2016	version 0.0.1i		Day restriction added, code clean up, UI changes
+ *		10/20/2016	version 0.0.1h		Mode restriction working
+ *  	10/18/2016	version 0.0.1g		Access Token Reset fixed. Multiple UI Changes.  **VERIFY YOUR PROFILES AFTER UPDATE**
+ *  	10/17/2016	version 0.0.1f		bug fixes. Sonos, media player, speech synthesizer working.  **SONOS STILLS NEEDS TESTING**
+ *		10/17/2016 	version 0.0.1e 		bug fixes.
+ *  	10/15-2016	version 0.0.1d		Added custom "Pre-messages", UI changes
+ * 		10/14/2016 	version 0.0.1c		Added Sonos support and OAuth tokens to logs for copy and paste
+ *		10/11/2016	version 0.0.1b		Fixed audio output for both media and synth
+ *		10/10/2016 	Version 0.0.1a		Added media player support
  *		10/09/2016	Version 0.0.1		Initial File
  *
  /******************* ROADMAP ********************
   - External TTS
+  - Icons
  *
  *
  *  Copyright 2016 Jason Headley
@@ -95,7 +106,7 @@ def pageSpeech(){
 def pageTextMessage(){
 	dynamicPage(name: "pageTextMessage", uninstall: false) {
     	section (""){ 
-    	input "sendContactText", "bool", title: "Enable Text Notifications to Contact Book (if available)", required: true, default: false, submitOnChange: true
+    	input "sendContactText", "bool", title: "Enable Text Notifications to Contact Book (if available)", required: true, submitOnChange: true
         	if (sendContactText) input "recipients", "contact", title: "Send text notifications to (optional)", multiple: true, required: false
          }       	
          section ( "" ){            
@@ -103,7 +114,7 @@ def pageTextMessage(){
              if (sendText){      
                     paragraph "You may enter multiple phone numbers separated by semicolon to deliver the Alexa message as a text and a push notification. E.g. 8045551122;8046663344"
                 	input name: "sms", title: "Send text notification to (optional):", type: "phone", required: false
-					input "push", "bool", title: "Send Push Notification (optional)", required: false, defaultValue: false
+					input "push", "bool", title: "Send Push Notification (optional)", required: false, defaultValue: true
             	}            
 			}
 		 section ( "" ){            
