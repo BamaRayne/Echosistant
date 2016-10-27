203584d1c9d5a5f041f7353eb304431a9d7dc0ab
Bamarayne <alabamaslammer21@gmail.com>
2016-10-26 23:29:52 -0500

    Update echosistant.groovy

--- echosistant.groovy
+++ echosistant.groovy
@@ -5,22 +5,11 @@
  *		10/26/2016  version 0.1.2a      Name change
  *		10/26/2016	version 0.1.2		Added Icons by @SBDOBRESCU.
  *		10/25/2016	version 0.1.1		Bug Fix in the Token Renew process
- *  	10/25/2016  version 0.1.0		Alpha Testing of code complete.
- *		10/23/2016	version 0.0.2		Restrictions operational (modes/days/hours), Sonos operational.		
- *		10/23/2016	version 0.0.1i		Day restriction added, code clean up, UI changes
- *		10/20/2016	version 0.0.1h		Mode restriction working
- *  	10/18/2016	version 0.0.1g		Access Token Reset fixed. Multiple UI Changes.  **VERIFY YOUR PROFILES AFTER UPDATE**
- *  	10/17/2016	version 0.0.1f		bug fixes. Sonos, media player, speech synthesizer working.  **SONOS STILLS NEEDS TESTING**
- *		10/17/2016 	version 0.0.1e 		bug fixes.
- *  	10/15-2016	version 0.0.1d		Added custom "Pre-messages", UI changes
- * 		10/14/2016 	version 0.0.1c		Added Sonos support and OAuth tokens to logs for copy and paste
- *		10/11/2016	version 0.0.1b		Fixed audio output for both media and synth
- *		10/10/2016 	Version 0.0.1a		Added media player support
+ *		10/25/2016  version 0.1.0		Alpha Testing of code complete.
  *		10/09/2016	Version 0.0.1		Initial File
  *
  /******************* ROADMAP ********************
   - External TTS
-  - Icons
  *
  *
  *  Copyright 2016 Jason Headley
