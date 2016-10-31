91a26cff6a9bedda9d2f3df44d52b281ff27d81a
sbdobrescu <sbdobrescu@gmail.com>
2016-10-31 12:06:10 -0400

    Update AssistantParent.groovy

--- AssistantParent.groovy
+++ AssistantParent.groovy
@@ -17,115 +17,197 @@
  *
  */
 definition(
-    name		: "Assistant",
-    namespace	: "sb",
-    author		: "Jason Headley",
-    description	: "The parent app.",
-    category	: "My Apps",
-    iconUrl		: "https://raw.githubusercontent.com/BamaRayne/alexaspeaks.src/master/app-Echosistant.png",
-    iconX2Url	: "https://raw.githubusercontent.com/BamaRayne/alexaspeaks.src/master/app-Echosistant@2x.png",
-    iconX3Url	: "https://raw.githubusercontent.com/BamaRayne/alexaspeaks.src/master/app-Echosistant@2x.png")
+name		: "Echosistant",
+namespace	: "Echo",
+author		: "JH",
+description	: "The Ultimate Voice Assistant.",
+category	: "My Apps",
+iconUrl		: "https://raw.githubusercontent.com/BamaRayne/Echosistant/master/smartapps/bamarayne/echosistant.src/app-Echosistant.png",
+iconX2Url	: "https://raw.githubusercontent.com/BamaRayne/Echosistant/master/smartapps/bamarayne/echosistant.src/app-Echosistant@2x.png",
+iconX3Url	: "https://raw.githubusercontent.com/BamaRayne/Echosistant/master/smartapps/bamarayne/echosistant.src/app-Echosistant@2x.png")
+
 preferences {
-	page name: "main"
-    page name: "About"
-    page name: "Reset"
-    page name: "Confirmation"
-}
-def installed() {
-log.debug "Installed with settings: ${settings}"
-initialize()
-log.debug "there are ${childApps.size()} child smartapps"
-    childApps.each {child ->
-        log.debug "child app: ${child.label}"
+	page(name: "mainPage", title: "Echosistant", install: true, uninstall: false) {
+		section {
+			app(name: "Profiles", appName: "echosistantProfile", namespace: "Echo", description: "Create New Profile...", multiple: true)
+		}
+		section {
+			href "about", title: "About Echosistant", description: "Tap here for details about Access Token and more..."
+		} 
+	} 
+} 
+
+page name: "about"
+	def about(){
+		dynamicPage(name: "about", uninstall: true) {
+			section {
+				def msg = state.accessToken != null ? state.accessToken : "Could not create Access Token. OAuth may not be enabled. "+
+				"Go to the SmartApp IDE settings to enable OAuth."				 
+                paragraph "${textAppName()}\n${textVersion()}\n${textCopyright()}",
+                image: "https://raw.githubusercontent.com/BamaRayne/alexaspeaks.src/master/app-Echosistant.png"
+				OAuthToken()
+				paragraph "Access token:\n${msg}\n\nApplication ID:\n${app.id}"
+			}
+			section ("Access Token / Application ID"){
+				href "Tokens", title: "Show Access Token and Application ID. Renew/Reset Token", description: none
+			}
+			section ("Apache License"){
+				input "ShowLicense", "bool", title: "Show License", default: false, submitOnChange: true
+				def msg = textLicense()
+					if (ShowLicense) paragraph "${msg}"
+			}
+			section("Instructions") { paragraph ("For detailed installation and how-to instruction, follow the link below") 
+			}
+			section("Tap below to remove the ${textAppName()} application"){}
+			}
 	}
-}    
+
+page name: "Tokens"
+	def Tokens(){
+		dynamicPage(name: "Tokens", title: "Security Tokens", uninstall: false){
+			section{
+				paragraph "The Access Token and App ID are now displayed in your Live Logging tab of the IDE website."
+			}
+			section("Setup Data for Developer Site"){
+				paragraph "Tap below to Reset/Renew the Security Token. You must log in to the IDE and open the Live Logs tab before tapping here. "+
+				"Copy and paste the displayed tokens into your Amazon Lambda Code."
+				if (!state.accessToken) {
+                	OAuthToken()
+					paragraph "You must enable OAuth via the IDE to setup this app"
+				}
+            }
+	def msg = state.accessToken != null ? state.accessToken : "Could not create Access Token. "+
+    "OAuth may not be enabled. Go to the SmartApp IDE settings to enable OAuth." 
+	log.trace "STappID = '${app.id}' , STtoken = '${state.accessToken}'"
+			section ("Reset Access Token / Application ID"){
+				href "pageConfirmation", title: "Reset Access Token and Application ID", description: none
+			}
+		}
+	} 
+
+page name: "pageReset"
+	def pageReset(){
+		dynamicPage(name: "pageReset", title: "Access Token Reset", uninstall: false){
+			section{
+				revokeAccessToken()
+				state.accessToken = null
+				OAuthToken()
+				def msg = state.accessToken != null ? "New access token:\n${state.accessToken}\n\nClick 'Done' above to return to the previous menu." : "Could not reset Access Token."+
+            	"OAuth may not be enabled. Go to the SmartApp IDE settings to enable OAuth."
+				paragraph "${msg}"
+			}
+			section(" "){ 
+        		href "mainPage", title: "Tap Here To Go Back To Main Menu", description: none 
+        	}
+		}
+	}
+    
+page name: "pageConfirmation"
+	def pageConfirmation(){
+		dynamicPage(name: "pageConfirmation", title: "Reset/Renew Access Token Confirmation", uninstall: false){
+			section {
+				href "pageReset", title: "Reset/Renew Access Token", description: "Tap here to confirm action - READ WARNING BELOW"
+				paragraph "PLEASE CONFIRM! By resetting the access token you will disable the ability to interface this SmartApp with your Amazon Echo."+
+            	"You will need to copy the new access token to your Amazon Lambda code to re-enable access." +
+				"Tap below to go back to the main menu with out resetting the token. You may also tap Done above."
+			}
+			section(" "){
+        		href "mainPage", title: "Cancel And Go Back To Main Menu", description: none 
+        	}
+		}
+	}	
+
+
+//************************************************************************************************************
+mappings {
+      path("/r") {action: [GET: "readData"]}
+//      path("/w") {action: [GET: "writeData"]}
+      path("/t") {action: [GET: "processTts"]}
+      path("/b") { action: [GET: "processBegin"] }
+	  path("/u") { action: [GET: "getURLs"] }
+	  path("/setup") { action: [GET: "setupData"] }}
+//************************************************************************************************************
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	log.trace "STappID = '${app.id}' , STtoken = '${state.accessToken}'"
+	initialize()
+}
 def updated() {
-log.debug "Updated with settings: ${settings}"
-initialize()
-unsubscribe()
+	log.debug "Updated with settings: ${settings}"
+	log.trace "STappID = '${app.id}' , STtoken = '${state.accessToken}'"
+	initialize()
+	unsubscribe()
 }
 def initialize() {
+	log.debug "there are ${childApps.size()} child smartapps"
+//childApps.each {child ->
+//log.debug "child app: ${child.label}"
 	if (!state.accessToken) {
 		log.error "Access token not defined. Ensure OAuth is enabled in the SmartThings IDE."
 	}
-}    
-def main(){
-	def installed = app.installationState == "COMPLETE"
-	return dynamicPage(
-    	name		: "main"
-        ,title		: "Profiles"
-        ,install	: true
-        ,uninstall	: installed
-        ){	
-            if (installed){
-        		section(){
-        			app(name: "Profiles", appName: "assistantProfile", namespace: "sb", description: "Create New Profile...", multiple: true)	
-            }
-               section("Security Tokens"){
-                	paragraph ("Log into the IDE on your computer and navigate to the Live Logs tab. Leave that window open, come back here and open this section")
-                    input "ShowTokens", "bool", title: "Show Security Tokens", default: false, submitOnChange: true
-                    def msg = state.accessToken  
-            		if (ShowTokens) paragraph "The Security Tokens are now displayed in the Live Logs section of the IDE"
-                    if (ShowTokens) paragraph "Access token:\n${msg}\n\nApplication ID:\n${app.id}"
-                    if (ShowTokens) log.trace "STappID = '${app.id}' , STtoken = '${state.accessToken}'"
-     	 	}
-		        section ("Apache License"){
-        			input "ShowLicense", "bool", title: "Show License", default: false, submitOnChange: true
-            		def msg1 = textLicense()
-            		if (ShowLicense) paragraph "${msg1}"
-     		}
-    			section("SmartApp Directions"){ 
-                	paragraph ("For detailed installation and how-to instruction, follow the link below") 
-        	}
-    	}		
-    }    	
 }
-  
-def about(){
-	dynamicPage(name: "about", uninstall: true) {
-        section {
-        	paragraph "${textAppName()}\n${textVersion()}\n${textCopyright()}",image: "https://raw.githubusercontent.com/BamaRayne/alexaspeaks.src/master/app-Echosistant.png"	//    if (!state.accessToken) OAuthToken()
-            def msg = state.accessToken != null ? state.accessToken : "Could not create Access Token. OAuth may not be enabled. Go to the SmartApp IDE settings to enable OAuth."
-            paragraph "Access token:\n${msg}\n\nApplication ID:\n${app.id}"
- 		}
-    	section ("Access Token / Application ID"){
-        	href "pageTokens", title: "Show Access Token and Application ID.  Renew/Reset Token", description: none
-      	}
-        section ("Apache License"){
-        	input "ShowLicense", "bool", title: "Show License", default: false, submitOnChange: true
-            def msg = textLicense()
-            if (ShowLicense) paragraph "${msg}"
-     	}
-    	section("Instructions") { paragraph ("For detailed installation and how-to instruction, follow the link below") 
-        }
-        section("Tap below to remove the ${textAppName()} application"){}
+//def writeData() {
+//    log.debug "Command received with params $params"
+//	}
+
+def readData() {
+    log.debug "Command received with params $params"
+   log.trace "Command = $params, intent = '${intent}' , tts = '${tts}'"
+   def tts = params.ttstext 
+        def txt = params.ttstext
+		def intentName = params.intentName    
+}
+
+//************************************************************************************************************
+def OAuthToken(){
+	try {
+		createAccessToken()
+		log.debug "Creating new Access Token"
+	} catch (e) {
+		log.error "Access Token not defined. OAuth may not be enabled. Go to the SmartApp IDE settings to enable OAuth."
 	}
 }
-    def OutputTxt;
-            if (outputTxt) {
-                url += 'g?data=' + data;
-                process = true;
-            } 
-private def textLicense() {
-	def text =
-		"Licensed under the Apache License, Version 2.0 (the 'License'); "+
-		"you may not use this file except in compliance with the License. "+
-		"You may obtain a copy of the License at"+
-		"\n\n"+
-		"    http://www.apache.org/licenses/LICENSE-2.0"+
-		"\n\n"+
-		"Unless required by applicable law or agreed to in writing, software "+
-		"distributed under the License is distributed on an 'AS IS' BASIS, "+
-		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. "+
-		"See the License for the specific language governing permissions and "+
-		"limitations under the License."
-}  
- def processTts() {
+//************************************************************************************************************
+
+//*** TEXT TO SPEECH PROCESS ***
+def processTts() {	
         def tts = params.ttstext 
         def txt = params.ttstext
-		def intent  = params.Param
-        tts = PreMsg + tts
-        log.trace "data"
-return ["outputTxt":outputTxt]
-	    
- } 
+		def intentName = params.intentName
+
+   log.trace "Command = $params, intent = '${intentName}' , tts = '${tts}'"
+
+}        
+
+//************************************************************************************************************
+
+//Version/Copyright/Information/Help
+private def textAppName() {
+	def text = "Echosistant"
+}	
+private def textVersion() {
+	def text = "Version 1.0.0 (10/29/2016)"
+}
+private def textCopyright() {
+	def text = "Copyright © 2016 Jason Headley"
+}
+private def textLicense() {
+	def text =
+	"Licensed under the Apache License, Version 2.0 (the 'License'); "+
+	"you may not use this file except in compliance with the License. "+
+	"You may obtain a copy of the License at"+
+	"\n\n"+
+	" http://www.apache.org/licenses/LICENSE-2.0"+
+	"\n\n"+
+	"Unless required by applicable law or agreed to in writing, software "+
+	"distributed under the License is distributed on an 'AS IS' BASIS, "+
+	"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. "+
+	"See the License for the specific language governing permissions and "+
+	"limitations under the License."
+}
+
+private def textHelp() {
+	def text =
+		"This smartapp allows you to speak freely to your Alexa device and have it repeated back on a remote playback device"h
+}
