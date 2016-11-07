b62515a3ee1def62d440096828113728d50bb9e8
sbdobrescu <sbdobrescu@gmail.com>
2016-11-07 13:39:06 -0500

    API path debug

--- smartapps/echo/echosistant.src/echosistant.groovy
+++ smartapps/echo/echosistant.src/echosistant.groovy
@@ -76,6 +76,7 @@ page name: "about"
             	def msg = state.accessToken != null ? state.accessToken : "Could not create Access Token. OAuth may not be enabled. "+
 				"Go to the SmartApp IDE settings to enable OAuth."	
                 if (ShowTokens) log.info "STappID = '${app.id}' , STtoken = '${state.accessToken}'"
+		if (ShowTokens) log.info " API url: ${getApiServerUrl()}/api/smartapps/installations/${app.id}/cheat?access_token=${state.accessToken}"
                 if (ShowTokens) paragraph "Access token:\n${msg}\n\nApplication ID:\n${app.id}"
 			}
 			section ("Revoke/Renew Access Token & Application ID"){
@@ -111,7 +112,8 @@ page name: "Tokens"
             }
 					def msg = state.accessToken != null ? state.accessToken : "Could not create Access Token. "+
     				"OAuth may not be enabled. Go to the SmartApp IDE settings to enable OAuth." 
-					log.trace "STappID = '${app.id}' , STtoken = '${state.accessToken}'"
+					log.info "STappID = '${app.id}' , STtoken = '${state.accessToken}'"
+					log.info " API url: ${getApiServerUrl()}/api/smartapps/installations/${app.id}/cheat?access_token=${state.accessToken}"
 			section ("Reset Access Token / Application ID"){
 				href "pageConfirmation", title: "Reset Access Token and Application ID", description: none
 		}
@@ -143,6 +145,7 @@ page name: "pageReset"
 				paragraph "${msg}"
                 paragraph "The new access token and app ID are now displayed in the Live Logs tab of the IDE."
                 log.info "STappID = '${app.id}' , STtoken = '${state.accessToken}'"
+		log.info " API url: ${getApiServerUrl()}/api/smartapps/installations/${app.id}/cheat?access_token=${state.accessToken}"
 			}
 			section(" "){ 
         		href "mainPage", title: "Tap Here To Go Back To Main Menu", description: none 
@@ -157,6 +160,7 @@ mappings {
 def installed() {
 	if (debug) log.debug "Installed with settings: ${settings}"
 	if (debug) log.trace "STappID = '${app.id}' , STtoken = '${state.accessToken}'"
+	if (debug) log.info " API url: ${getApiServerUrl()}/api/smartapps/installations/${app.id}/cheat?access_token=${state.accessToken}"
 	initialize()
 }
 def updated() {
@@ -174,7 +178,8 @@ children.each { child ->
 	if (!state.accessToken) {
                OAuthToken()
 		paragraph "You must enable OAuth via the IDE to setup this app"
-		log.trace "STappID = '${app.id}' , STtoken = '${state.accessToken}'"            
+		log.trace "STappID = '${app.id}' , STtoken = '${state.accessToken}'"  
+		log.info " API url: ${getApiServerUrl()}/api/smartapps/installations/${app.id}/cheat?access_token=${state.accessToken}"
 		}
     }
 
