9ff59c0034daff0a0539c35605f07b116f586206
Bamarayne <alabamaslammer21@gmail.com>
2016-11-23 15:43:38 -0600

    Lambda ver 3.0 final version

--- smartapps/bamarayne/echosistant.src/echosistant-lambda.js
+++ smartapps/bamarayne/echosistant.src/echosistant-lambda.js
@@ -1,3 +1,32 @@
+/**
+ *  EchoSistant - Lambda Code
+ *
+ *  Version 3.0.0 - 11/21/2016 Copyright © 2016 Jason Headley
+ *  Special thanks for Michael Struck @MichaelS (Developer of AskAlexa) for allowing me
+ *  to build off of his base code.  Special thanks to Keith DeLong  @N8XD for his 
+ *  assistance in troubleshooting.... as I learned.....  Special thanks to Bobby
+ *  @SBDOBRESCU for jumping on board and being a co-consipirator in this adventure.
+ *  
+ *  Version 2.0.0 - 11/20/2016  Continued Commands
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+'use strict';
+exports.handler = function( event, context ) {
+    var https = require( 'https' );
+    // Paste app code here between the breaks------------------------------------------------
+    var STappID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    var STtoken = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    var url='https://graph.api.smartthings.com:443/api/smartapps/installations/' + STappID + '/' ;
+        //---------------------------------------------------------------------------------------
         var cardName ="";
         var stop;
         var areWeDone = false;
@@ -70,7 +99,7 @@ else if (ttstext=="sure") {
 else if (ttstext === "repeat"+"last"+"message") {
                 url += 't?ttstext=' + ttstext + '&intentName=' + intentName;
                 process = true;
-                areWeDone = true;
+                areWeDone = false;
 }
 else if (intentName) {
                 url += 't?ttstext=' + ttstext + '&intentName=' + intentName;
@@ -88,10 +117,7 @@ else {
                     var pContCmds = resJSON.pContCmds;
                     var speechText = resJSON.outputTxt;
                     console.log(speechText);
-                    if (ttstext == "repeat last message") {
-                        areWeDone=true;
-                    }
-                    else if (pContCmds === true) { 
+                    if (pContCmds === true) { 
                         areWeDone=false;
                         speechText = speechText + ', send another message?'; 
                     }
