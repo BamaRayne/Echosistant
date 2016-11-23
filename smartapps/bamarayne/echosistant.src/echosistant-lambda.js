f227be1e5f1cc78c7a78211c2d9dd311dbb8dfef
Bamarayne <alabamaslammer21@gmail.com>
2016-11-23 16:23:11 -0600

    EchoSistant Lambda Ver. 3.0

--- smartapps/bamarayne/echosistant.src/echosistant-lambda.js
+++ smartapps/bamarayne/echosistant.src/echosistant-lambda.js
@@ -1,17 +1,14 @@
 /**
  *  EchoSistant - Lambda Code
  *
- *  Version 1.0.1 - 11/05/2016 Name fix in code
- *  Version 1.0.0 - 11/04/2016 
- *
- *  Copyright © 2016 Jason Headley
- *
- *
+ *  Version 3.0.0 - 11/21/2016 Copyright © 2016 Jason Headley
  *  Special thanks for Michael Struck @MichaelS (Developer of AskAlexa) for allowing me
  *  to build off of his base code.  Special thanks to Keith DeLong  @N8XD for his 
  *  assistance in troubleshooting.... as I learned.....  Special thanks to Bobby
  *  @SBDOBRESCU for jumping on board and being a co-consipirator in this adventure.
  *  
+ *  Version 2.0.0 - 11/20/2016  Continued Commands
+ * 
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
@@ -22,43 +19,116 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
-
 'use strict';
-    exports.handler = function( event, context ) {
+exports.handler = function( event, context ) {
     var https = require( 'https' );
     // Paste app code here between the breaks------------------------------------------------
-    var STappID = 'xxxxxxx';
-    var STtoken = 'xxxxxxx';
+    var STappID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    var STtoken = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
     var url='https://graph.api.smartthings.com:443/api/smartapps/installations/' + STappID + '/' ;
-    //---------------------------------------------------------------------------------------
+        //---------------------------------------------------------------------------------------
         var cardName ="";
+        var stop;
+        var areWeDone = false;
         var endSession = true;
         var processedText;
         var process = false;
         var intentName = event.request.intent.name;
         var ttstext = event.request.intent.slots.ttstext.value;
         var ttsintentname = event.request.intent.slots.ttstext.name.value;
+        var ttsTxt;
         var speechText;
         var outputTxt;
-            if (intentName) {
+        var pContCmds;
+        var cancel;
+        var no;
+     //   var repeat = "";
+console.log (event.request.type);
+if (ttstext=="stop") {
+areWeDone=true;
+output(" Stopping. Goodbye ", context, "Amazon Stop", areWeDone);
+} 
+else if (ttstext=="no") {
+areWeDone=true;
+output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
+}
+else if (ttstext=="nope") {
+areWeDone=true;
+output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
+}
+else if (ttstext=="no thank you") {
+areWeDone=true;
+output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
+}
+else if (ttstext=="no we're done") {
+areWeDone=true;
+output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
+}
+else if (ttstext=="no we're good") {
+areWeDone=true;
+output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
+}
+else if (ttstext=="no I'm done") {
+areWeDone=true;
+output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
+}
+else if (ttstext=="no thanks") {
+areWeDone=true;
+output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
+}
+else if (ttstext=="cancel") {
+areWeDone=true;
+output(" Cancelling. Goodbye ", context, "Amazon Stop", areWeDone);
+}
+else if (ttstext=="yes") {
+        areWeDone=false;    
+    output("please continue...", context, areWeDone);
+}
+else if (ttstext=="okay") {
+        areWeDone=false;
+    output("please continue...", context, areWeDone);
+}
+else if (ttstext=="yeah") {
+        areWeDone=false;
+    output("please continue...", context, areWeDone);
+}
+else if (ttstext=="sure") {
+        areWeDone=false;
+    output("please continue...", context, areWeDone);
+}
+else if (ttstext === "repeat"+"last"+"message") {
                 url += 't?ttstext=' + ttstext + '&intentName=' + intentName;
                 process = true;
-            } 
-            if (!process) ("I'm sorry, I did not understand what you are requesting. please try again",context);
-               else {
+                areWeDone = false;
+}
+else if (intentName) {
+                url += 't?ttstext=' + ttstext + '&intentName=' + intentName;
+                process = true;
+}
+if (!process) {
+areWeDone=true;
+output("I am not sure what you are asking. Please try again", context, areWeDone); 
+}
+else {
                     url += '&access_token=' + STtoken;
                     https.get( url, function( response ) {
                     response.on( 'data', function( data ) {
                     var resJSON = JSON.parse(data);
-                    var speechText = "The SmartThings SmartApp returned an error. I was unable to complete your request";
+                    var pContCmds = resJSON.pContCmds;
+                    var speechText = resJSON.outputTxt;
                     console.log(speechText);
-                output(resJSON.outputTxt, context, cardName);
+                    if (pContCmds === true) { 
+                        areWeDone=false;
+                        speechText = speechText + ', send another message?'; 
+                    }
+                    else {
+                        areWeDone=true;
+                    }
+                    output(speechText, context, cardName, areWeDone);
                 } );
             } );
         }
-    };
-        
-            function output( text, context ) {
+function output( text, context ) {
             var response = {
              outputSpeech: {
              type: "PlainText",
@@ -69,7 +139,8 @@
                  title: "EchoSistant Smartapp",
                  content: text
                     },
-                    shouldEndSession: true
+                    shouldEndSession: areWeDone
                     };
                     context.succeed( { response: response } );
-                    }
+  }
+};
