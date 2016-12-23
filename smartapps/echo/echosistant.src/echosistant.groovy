1624fe8dcebe6c778d7becf0614502cc32b6acbd
Bamarayne <alabamaslammer21@gmail.com>
2016-12-23 15:18:38 -0600

    EchoSistant Groovy Ver 3.0 Release 12/23/2016

--- smartapps/echo/echosistant.src/echosistant.groovy
+++ smartapps/echo/echosistant.src/echosistant.groovy
@@ -848,6 +848,7 @@ def initialize() {
         if (parent.debug) log.debug "Initializing Child app"
         state.lastMessage = null
     	state.lastTime  = null
+        state.recording = null
         subscribeChildToEvents()
      }
      else{
@@ -1420,7 +1421,8 @@ def feedbackHandler(data) {
    TEXT TO SPEECH PROCESS (PARENT) - Lambda via page t
 ************************************************************************************************************/
 def processTts() {
-		def ptts = params.ttstext 
+		def ptts = params.ttstext
+        
 		def pttx = params.ttstext 
         def pintentName = params.intentName
             if (debug) log.debug "Message received from Lambda with: (ptts) = '${ptts}', (pintentName) = '${pintentName}'"
@@ -1428,12 +1430,21 @@ def processTts() {
     	def pContCmds = "false"
         def pContCmdsR = "false"
         def dataSet = [ptts:ptts,pttx:pttx,pintentName:pintentName] 
-        def repeat = "repeat last message" 
-        if (ptts==repeat) {
+        	def repeat = "repeat last message" 
+                    	log.debug "repeat = ${repeat}"
+
+        	def play = "play message"
+                    	log.debug "play = ${play}"
+
+        	def record = ptts.replace("record a message", "")
+        				log.debug "record = ${record}"
+        	def recordingNow = ptts.startsWith("record a message")
+        				log.debug "recordingNow = ${recordingNow}"
+      
+        if (ptts==repeat || ptts == play) {
 						childApps.each { child ->
     						def cLast = child.label.toLowerCase()
             				if (cLast == pintentName.toLowerCase()) {
-                        		if (debug) log.debug "Last Child was = '${cLast}'"  
                                 def cLastMessage 
                        			def cLastTime
                                 pContCmds = child.ContCmds
@@ -1441,7 +1452,10 @@ def processTts() {
                                 if (pContCmdsR == false) {
                                 	pContCmds = pContCmdsR
                                 }
-                                outputTxt = child.getLastMessage()
+                                if (ptts==repeat) {
+                                	outputTxt = child.getLastMessage()
+                                }
+                                else outputTxt = "Your last recording was, " + state.recording
                                 if (debug) log.debug "Profile matched is ${cLast}, last profile message was ${outputTxt}" 
                 			}
                			}
@@ -1450,10 +1464,9 @@ def processTts() {
     			if (ptts){
      				state.lastMessage = ptts
                     state.lastIntent = pintentName
-                    state.lastTime = new Date(now()).format("h:mm aa", location.timeZone)
-                    if (debug) log.debug "Running main loop with '${ptts}' ...enabling debug logs for the Profile"                      
+                    state.lastTime = new Date(now()).format("h:mm aa", location.timeZone)                   
                     childApps.each {child ->
-						child.profileEvaluate(dataSet)
+						if (recordingNow == false) child.profileEvaluate(dataSet)
                         }
             			//Preparing Alexa Response
                         childApps.each { child ->
@@ -1463,7 +1476,14 @@ def processTts() {
 									def cArepeat = child.Arepeat
 									def cAfeedBack = child.AfeedBack
                                     pContCmds = child.ContCmds
-     	                			if (cAfeedBack != true) {
+
+                                    if (recordingNow == true) {
+        								state.recording = record
+        								outputTxt = "Ok, message recorded. To play it later, just say: play message to this profile"
+                                        pContCmds = "false"
+        							}
+                                    
+     	                			else (cAfeedBack != true) {
                                 		if (cAcustom != false) {
                             				outputTxt = child.outputTxt
                             			}
