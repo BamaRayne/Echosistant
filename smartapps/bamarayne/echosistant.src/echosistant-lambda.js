90836a45a86fa26fbc470b5f2e48bf9cbe3f3f36
Bamarayne <alabamaslammer21@gmail.com>
2016-12-23 14:57:01 -0600

    Lambda 3.0 Release 12/23/2016

--- smartapps/bamarayne/echosistant.src/echosistant-lambda.js
+++ smartapps/bamarayne/echosistant.src/echosistant-lambda.js
@@ -1,12 +1,16 @@
 /**
  *  EchoSistant - Lambda Code
  *
- *  Version 3.0.0 - 11/21/2016 Copyright © 2016 Jason Headley
+ *  Copyright © 2016 Jason Headley
  *  Special thanks for Michael Struck @MichaelS (Developer of AskAlexa) for allowing me
  *  to build off of his base code.  Special thanks to Keith DeLong  @N8XD for his 
  *  assistance in troubleshooting.... as I learned.....  Special thanks to Bobby
  *  @SBDOBRESCU for jumping on board and being a co-consipirator in this adventure.
- *  
+ *
+ *  Version 3.1.3 - 12/19/2016 
+ *  Version 3.1.2 - 12/12/2016
+ *  Version 3.1.0 - 12/7/2016
+ *  Version 3.0.0 - 12/1/2016  Added new parent variables
  *  Version 2.0.0 - 11/20/2016  Continued Commands
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
@@ -22,125 +26,119 @@
 'use strict';
 exports.handler = function( event, context ) {
     var https = require( 'https' );
-    // Paste app code here between the breaks------------------------------------------------
+//-------- Paste app code here between the breaks------------------------------------------------
     var STappID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
     var STtoken = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
     var url='https://graph.api.smartthings.com:443/api/smartapps/installations/' + STappID + '/' ;
-        //---------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
         var cardName ="";
-        var stop;
-        var areWeDone = false;
-        var endSession = true;
-        var processedText;
-        var process = false;
-        var intentName = event.request.intent.name;
-        var ttstext = event.request.intent.slots.ttstext.value;
-        var ttsintentname = event.request.intent.slots.ttstext.name.value;
-        var ttsTxt;
-        var speechText;
-        var outputTxt;
-        var pContCmds;
-        var cancel;
-        var no;
-     //   var repeat = "";
-console.log (event.request.type);
-if (ttstext=="stop") {
-areWeDone=true;
-output(" Stopping. Goodbye ", context, "Amazon Stop", areWeDone);
-} 
-else if (ttstext=="no") {
-areWeDone=true;
-output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
-}
-else if (ttstext=="nope") {
-areWeDone=true;
-output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
-}
-else if (ttstext=="no thank you") {
-areWeDone=true;
-output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
-}
-else if (ttstext=="no we're done") {
-areWeDone=true;
-output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
-}
-else if (ttstext=="no we're good") {
-areWeDone=true;
-output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
-}
-else if (ttstext=="no I'm done") {
-areWeDone=true;
-output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
-}
-else if (ttstext=="no thanks") {
-areWeDone=true;
-output(" It has been my pleasure.  Goodbye ", context, "Amazon Stop", areWeDone);
-}
-else if (ttstext=="cancel") {
-areWeDone=true;
-output(" Cancelling. Goodbye ", context, "Amazon Stop", areWeDone);
-}
-else if (ttstext=="yes") {
-        areWeDone=false;    
-    output("please continue...", context, areWeDone);
-}
-else if (ttstext=="okay") {
-        areWeDone=false;
-    output("please continue...", context, areWeDone);
-}
-else if (ttstext=="yeah") {
-        areWeDone=false;
-    output("please continue...", context, areWeDone);
-}
-else if (ttstext=="sure") {
-        areWeDone=false;
-    output("please continue...", context, areWeDone);
-}
-else if (ttstext === "repeat"+"last"+"message") {
-                url += 't?ttstext=' + ttstext + '&intentName=' + intentName;
-                process = true;
-                areWeDone = false;
-}
-else if (intentName) {
-                url += 't?ttstext=' + ttstext + '&intentName=' + intentName;
-                process = true;
-}
-if (!process) {
-areWeDone=true;
-output("I am not sure what you are asking. Please try again", context, areWeDone); 
-}
-else {
+        var areWeDone = true;
+//-------- Validation process and begining interaction with SmartThings app-------------------- 
+        var versionTxt = '3.0';
+        var versionDate= '12/23/2016';
+        var releaseTxt = "3.1.5";
+        var beginURL = url + 'b?&versionTxt=' + versionTxt + '&versionDate=' + versionDate + '&releaseTxt=' + releaseTxt + '&access_token=' + STtoken;
+        https.get( beginURL, function( response ) {
+        response.on( 'data', function( data ) {
+            var startJSON = JSON.parse(data);
+            var contOptions = startJSON.pContinue; //setting global variable if continuation is allowed for either control or tts
+            var verST = startJSON.versionSTtxt;
+//-------- Error trapping--------------------------------------------------------------------
+            if (startJSON.error) { 
+                output("There was an error. If this continues to happen, please reach out for help", context, "Lambda Error", areWeDone); 
+            }
+            if (startJSON.error === "invalid_token" || startJSON.type === "AccessDenied") {
+                output("There was an error accessing the SmartThings cloud environment. Please check your security token and application ID and try again. ", context, "Lambda Error", areWeDone); 
+            }
+            if (verST != versionTxt) { 
+                output("You are using outdated smart apps. Please make sure to update both the Lambda code and the SmartThings code to most recent versions, and then try again.", context, "Lambda Error", areWeDone);
+            }
+//-------- Begining Request------------------------------------------------------------------            
+            if (event.request.type == "LaunchRequest") { 
+                alexaResp ("LaunchRequest", context, areWeDone); 
+            }
+            else if (event.request.type == "SessionEndedRequest"){}
+            else if (event.request.type == "IntentRequest") {
+                var process = false;
+                var intentName = event.request.intent.name;
+                if (intentName.startsWith("AMAZON") && intentName.endsWith("Intent")) { 
+                    alexaResp (intentName, context, "Amazon Intent", areWeDone); 
+                }
+//-------- Control Type Request------------------------------------------------------------------
+                else if (intentName == "main"){           
+                    var pCommand = event.request.intent.slots.pCommand.value;
+                    var pProfile = event.request.intent.slots.pProfile.value;
+                    var pNum = event.request.intent.slots.pNum.value;
+                    var pDevice = event.request.intent.slots.pDevice.value;
+                    var pUnit = event.request.intent.slots.pUnit.value;
+                    url += 'c?pDevice=' + pDevice + '&pCommand=' + pCommand + '&pNum=' + pNum + '&pUnit=' + pUnit + '&pProfile=' + pProfile + '&intentName=' + intentName;    
+                    process = true;
+                    cardName = "EchoSistant Control";
+                }
+//-------- TTS Type Request------------------------------------------------------------------
+                else if (intentName != "main") {
+                    var ttstext = event.request.intent.slots.ttstext.value;
+                    url += 't?ttstext=' + ttstext + '&intentName=' + intentName;
+                    process = true;
+                    cardName = "EchoSistant Free Text";
+                }
+//-------- General Response------------------------------------------------------------------
+                if (!process) {
+                    output("I am not sure what you are asking. Please try again", context, areWeDone); 
+                }                                        
+                else {
                     url += '&access_token=' + STtoken;
                     https.get( url, function( response ) {
-                    response.on( 'data', function( data ) {
-                    var resJSON = JSON.parse(data);
-                    var pContCmds = resJSON.pContCmds;
-                    var speechText = resJSON.outputTxt;
-                    console.log(speechText);
-                    if (pContCmds === true) { 
-                        areWeDone=false;
-                        speechText = speechText + ', send another message?'; 
-                    }
-                    else {
-                        areWeDone=true;
-                    }
-                    output(speechText, context, cardName, areWeDone);
-                } );
-            } );
-        }
-function output( text, context ) {
-            var response = {
+                        response.on( 'data', function( data ) {
+                        var resJSON = JSON.parse(data);
+                        var pContCmds = resJSON.pContCmds;
+                        var speechText = resJSON.outputTxt;
+                        if (pContCmds === true) { 
+                            areWeDone=false;
+                            speechText = speechText + ', send another message to' + intentName; 
+                        }
+                        else {
+                            areWeDone=true;
+                        }
+                        output(speechText, context, cardName, areWeDone);
+                        } );
+                    } );
+                }
+            }
+        } );
+    } );
+};
+    
+function alexaResp(type, context, cardName, areWeDone){
+    if (type == "AMAZON.YesIntent") { 
+        areWeDone=false;
+        output("Please continue...", context, "EchoSistant Stop", areWeDone);
+    }
+    else if (type == "AMAZON.NoIntent") { 
+        areWeDone=true;
+        output(" It has been my pleasure.  Goodbye ", context, "EchoSistant Stop", areWeDone);
+    }
+    else if (type == "AMAZON.StopIntent") { 
+        areWeDone=true;
+        output(" Cancelling. Goodbye ", context, "EchoSistant Cancel", areWeDone);
+    }
+    else if (type == "AMAZON.CancelIntent") { 
+        areWeDone=true;
+        output(" Cancelling. Goodbye ", context, "EchoSistant Cancel", areWeDone);
+    }
+}
+function output( text, context, cardName, areWeDone) {
+        var response = {
              outputSpeech: {
              type: "PlainText",
              text: text
                  },
                  card: {
                  type: "Simple",
-                 title: "EchoSistant Smartapp",
+                 title: cardName,
                  content: text
                     },
-                    shouldEndSession: areWeDone
-                    };
-                    context.succeed( { response: response } );
+        shouldEndSession: areWeDone
+        };
+        context.succeed( { response: response } );
   }
-};
