bf427abb96b3f7340752db9e602466d8912b575d
Jason Headley <alabamaslammer21@gmail.com>
2017-03-13 12:11:12 -0500

    Version:4.0 R.0.2.12

--- smartapps/echo/echosistant.src/echosistant.groovy
+++ smartapps/echo/echosistant.src/echosistant.groovy
@@ -7,7 +7,7 @@
  
  ************************************ FOR INTERNAL USE ONLY ******************************************************
  *
- *		3/13/2017		Version:4.0 R.0.2.11	Bug fix for Weather forcast
+ *		3/13/2017		Version:4.0 R.0.2.12	Bug fix for Weather forcast
  *		3/12/2017		Version:4.0 R.0.2.11	Changed SHM status from Armed Stay to Armed Home for audio output
  *		3/08/2017		Version:4.0 R.0.2.10	enabled Profile Messaging retrieval
  *		3/08/2017		Version:4.0 R.0.2.9		bug fixes 
@@ -47,7 +47,7 @@ definition(
 	UPDATE LINE 38 TO MATCH RECENT RELEASE
 **********************************************************************************************************************************************/
 private release() {
-	def text = "R.0.2.11"
+	def text = "R.0.2.12"
 }
 /**********************************************************************************************************************************************/
 preferences {   
